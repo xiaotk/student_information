551749732dce127e68ec8ae5fdce8a4f57aa16ac
xiaotk <111@qq.com>
2020-10-18 11:43:58 +0800

    s

---
+++ src/main/java/com/hncpu/student_information/student_information/StudentInformationApplication.java
@@ -0,0 +1,13 @@
+package com.hncpu.student_information.student_information;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentInformationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentInformationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hncpu/student_information/student_information/StudentInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hncpu.student_information.student_information;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentInformationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
